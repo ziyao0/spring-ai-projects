5d62d69c5da501201f0a4741ebc8f8a195198fd4
ziyao <1337891505@qq.com>
2025-03-17 15:57:34 +0800

    feat: spring-ai-alibaba使用案例

---
+++ spring-ai-alibaba-example/src/main/java/com/ziyao/ai/alibaba/AiBotApplication.java
@@ -0,0 +1,16 @@
+package com.ziyao.ai.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ziyao
+ * @link <a href="https://github.com/ziyao0">ziyao for github</a>
+ */
+@SpringBootApplication
+public class AiBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiBotApplication.class, args);
+    }
+}
